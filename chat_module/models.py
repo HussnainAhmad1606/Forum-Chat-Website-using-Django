8045bce57ac8e2f31bcbc9abe174d36a2f36f22b
HussnainAhmad1606 <hussnainahmad1606@gmail.com>
2021-01-23 20:11:43 +0500

    Add new model for storing user messages and register the model, create admin account for django administration, and done all migrations

--- chat_module/models.py
+++ chat_module/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class message(models.Model):
+	message_id = models.IntegerField(primary_key=True)
+	user = models.CharField(max_length=50)
+	message = models.CharField(max_length=500)
+	timestamp = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.user
