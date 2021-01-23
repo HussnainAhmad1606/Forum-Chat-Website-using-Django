f12e36cd669c55eeea254208626dd20d20d0ca3c
HussnainAhmad1606 <hussnainahmad1606@gmail.com>
2021-01-23 19:46:21 +0500

    Add Django templates, set pipline for chat app

--- chat_module/views.py
+++ chat_module/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def home(request):
+	return render(request, "index.html")
+
+def messages(request):
+	return render(request, "messages.html")
+
