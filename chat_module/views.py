581013c20d19157ee493fb3542ea92a089fdb867
HussnainAhmad1606 <hussnainahmad1606@gmail.com>
2021-01-23 20:32:54 +0500

    Add functionality to send message by using UI by enter user name in username field and message in message field

--- chat_module/views.py
+++ chat_module/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import message
+from django.contrib import messages
 
 
 # Create your views here.
@@ -8,10 +9,18 @@ def home(request):
 	return render(request, "index.html")
 
 def messages(request):
-	allMessages = message.objects.all()
-	print(allMessages)
-	context = {
-	'messages': allMessages
-	}
-	return render(request, "messages.html", context)
+	if request.method == "POST":
+		userMessage = request.POST.get("message")
+		user = request.POST.get("user")
+		addMessage = message(user=user, message=userMessage)
+		addMessage.save()
+		return redirect("messages")
+
+	else:
+		allMessages = message.objects.all()
+		print(allMessages)
+		context = {
+		'messages': allMessages
+		}
+		return render(request, "messages.html", context)
 
